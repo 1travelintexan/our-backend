063ff216fbbbc087ad3069d44dd737bf4ce4c1d3
Joshua <horheyinc@gmail.com>
2024-12-18 16:05:51 +0100

    vercel node 3 no build

--- config/index.js
+++ config/index.js
@@ -26,6 +26,7 @@ module.exports = (app) => {
       origin: "*",
     })
   );
+  //no build and *
   // In development environment the app logs
   app.use(logger("dev"));
   // To have access to `body` property in the request
