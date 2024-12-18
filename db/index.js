35d9bb5df9123b6bb1a993795e9e086e617c7724
Joshua <horheyinc@gmail.com>
2024-12-18 21:59:37 +0100

    our db link added with console.log

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI =
-  process.env.OUR_MONGODB_URI || "mongodb://127.0.0.1:27017/our-backend";
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/our-backend";
 console.log("here is our mongo db uri", process.env.OUR_MONGODB_URI);
 mongoose
   .connect(MONGO_URI)
