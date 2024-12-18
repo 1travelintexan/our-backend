de2b78f4bf5b72ed49b2a7494adf2e38a1b7b860
Joshua <horheyinc@gmail.com>
2024-12-18 13:02:55 +0100

    vercel.json 9

--- app.js
+++ app.js
@@ -24,10 +24,4 @@ app.use("/auth", authRoutes);
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
-// ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
-const PORT = process.env.PORT || 5005;
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
-});
-
 module.exports = app;
