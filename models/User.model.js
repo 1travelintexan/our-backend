d036c502a8aeef2ee4b90a5856e8d237a381123e
Joshua <horheyinc@gmail.com>
2024-12-18 11:36:59 +0100

    signup working locally

--- models/User.model.js
+++ models/User.model.js
@@ -3,20 +3,14 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    email: {
+    firstName: {
       type: String,
-      required: [true, "Email is required."],
-      unique: true,
-      lowercase: true,
-      trim: true,
     },
-    password: {
-      type: String,
-      required: [true, "Password is required."],
+    age: {
+      type: Number,
     },
-    name: {
+    lastName: {
       type: String,
-      required: [true, "Name is required."],
     },
   },
   {
