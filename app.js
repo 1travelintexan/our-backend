60ff5f94d4e78c4c0039a25bdc26768fcc9f5da8
Joshua <horheyinc@gmail.com>
2024-12-18 15:40:24 +0100

    trying new source new 2  headers

--- app.js
+++ app.js
@@ -23,5 +23,6 @@ app.use("/auth", authRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
+//testing
 
 module.exports = app;
