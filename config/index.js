c29c62b5bbda8ecf74adf05dd05d766e85c5faf6
Joshua <horheyinc@gmail.com>
2024-12-18 14:34:32 +0100

    origin test

--- config/index.js
+++ config/index.js
@@ -27,10 +27,8 @@ module.exports = (app) => {
       origin: [FRONTEND_URL],
     })
   );
-
   // In development environment the app logs
   app.use(logger("dev"));
-
   // To have access to `body` property in the request
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
