89cc0520a93e61b2890a37522903bac435c54c31
Joshua <horheyinc@gmail.com>
2024-12-19 08:48:54 +0100

    trying with process.env

--- db/index.js
+++ db/index.js
@@ -9,7 +9,7 @@ const MONGO_URI =
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/our-backend";
 console.log("here is our mongo db uri", process.env.OUR_MONGODB_URI);
 mongoose
-  .connect(MONGO_URI)
+  .connect(process.env.MONGODB_URI)
   .then((x) => {
     const dbName = x.connections[0].name;
     console.log(`Connected to Mongo! Database name: "${dbName}"`);
